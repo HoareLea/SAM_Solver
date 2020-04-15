a3cb1dc1d16db930d0a9f92187d6bc67277b3d17
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-04-15 11:19:03 +0100

    PanelFromeSnapSolver Component Updated (Max Distance for Aperture to Be Copied Updated)

--- SAM.Solver.Grasshopper/PanelFromSnapSolver.cs
+++ SAM.Solver.Grasshopper/PanelFromSnapSolver.cs
@@ -86,7 +86,7 @@ namespace SAM.Solver.Grasshopper
                 if (panel_Old == null)
                     continue;
 
-                Analytical.Panel panel_New = new Analytical.Panel(panel_Old.Guid, panel_Old, face3D);
+                Analytical.Panel panel_New = new Analytical.Panel(panel_Old.Guid, panel_Old, face3D, true, Core.Tolerance.MacroDistance, 0.3);
 
                 result.Add(panel_New);
 

87f510b25e85d66508783e7e506a0d9ac8bcb0db
Michal Dengusiak <tassimulation@gmail.com>
2020-04-23 19:31:35 +0100

    Update SnapSolverComponent_V3.cs

--- SAM.Solver.Grasshopper/SnapSolverComponent_V3.cs
+++ SAM.Solver.Grasshopper/SnapSolverComponent_V3.cs
@@ -39,9 +39,9 @@ namespace SAM.Solver.Grasshopper
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            //TODO: Find better Way to change tolerance
-            Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance = Core.Tolerance.MacroDistance;
-            Rhino.RhinoDoc.ActiveDoc.ModelUnitSystem = Rhino.UnitSystem.Meters;
+            ////TODO: Find better Way to change tolerance
+            //Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance = Core.Tolerance.MacroDistance;
+            //Rhino.RhinoDoc.ActiveDoc.ModelUnitSystem = Rhino.UnitSystem.Meters;
 
             List<GooPanel> Panels = new List<GooPanel>();
             List<GooPanel> FixedPanels = new List<GooPanel>();
